510de63916c2e8c8c0eef728a6e289e8bdd23744
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-02 22:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 import streamlit as st
-from scipy.optimize import fsolve
 import numpy as np
 
-st.title("Nonlinear Equation Solver")
+st.title("Linear Equation Solver")
 
-st.write("Enter a system of equations in the form of F(x, y, w) = 0")
+st.write("Enter a linear system of equations as matrices (e.g., '[3, -9; 2, 4] * [x; y] = [-42; 2]', '[1, -2, -1; 2, 2, -1; -1, -1, 2] * [x; y; z] = [6; 1; 1]'):")
 
-# User input for the equations
-equation1 = st.text_input("Equation 1 (e.g., 'x**2 + y**2 - 20 = 0'):")
-equation2 = st.text_input("Equation 2 (e.g., 'y - x**2 = 0'):")
-equation3 = st.text_input("Equation 3 (e.g., 'w + 5 - x*y = 0'):")
+# User input for the equation
+equation_input = st.text_input("Linear System of Equations:")
 
-def myFunction(z):
-    x = z[0]
-    y = z[1]
-    w = z[2]
+try:
+    # Split the input into matrices and vectors
+    equations = equation_input.split('=')
+    coefficients, constants = equations[0].strip(), equations[1].strip()
 
-    F = np.empty((3))
-    F[0] = eval(equation1)  # Parse and evaluate user input as the first equation
-    F[1] = eval(equation2)  # Parse and evaluate user input as the second equation
-    F[2] = eval(equation3)  # Parse and evaluate user input as the third equation
-    return F
+    # Parse matrices and vectors
+    A = np.array(eval(coefficients.replace('[', '').replace(']', ''))
 
-zGuess = np.array([1, 1, 1])
+    b = np.array(eval(constants.replace('[', '').replace(']', ''))
 
-if st.button("Solve Equations"):
-    z = fsolve(myFunction, zGuess)
-    st.write(f"Solutions for x, y, w: {z}")
+    # Solve the linear system of equations
+    x = np.linalg.solve(A, b)
+
+    st.write(f"Solutions for variables: {x}")
+except Exception as e:
+    st.error("Invalid Linear System of Equations")
