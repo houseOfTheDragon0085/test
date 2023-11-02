22072c158855db7d35da27426b11d3522e132842
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-02 21:17:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+import pandas as pd
+from sympy import symbols, Eq, solve
 
 st.title("Sample Streamlit App")
-st.write("This is a simple Streamlit app.")
 
-# Add more Streamlit components and functionality as needed.
+# Read the dataset
+@st.cache  # Use caching to improve performance
+def load_data():
+    data = pd.read_csv('data/dataset.csv')
+    return data
+
+data = load_data()
+
+st.header("Sample Dataset")
+st.write(data)
+
+st.header("Algebraic Equation Solver")
+st.write("Solve an equation of the form 'ax + b = 0'")
+
+# User input for the equation
+a = st.number_input("Enter the value of 'a':")
+b = st.number_input("Enter the value of 'b':")
+
+equation = Eq(a * symbols('x') + b, 0)
+
+if st.button("Solve Equation"):
+    try:
+        solution = solve(equation, symbols('x'))
+        st.write(f"Solutions for x: {solution}")
+    except Exception as e:
+        st.error("Invalid Equation")
