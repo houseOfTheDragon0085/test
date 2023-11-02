612dbf3e68c80eec4b78c7b667ca356d574515c1
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-02 21:33:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 import streamlit as st
-import pandas as pd
-from sympy import symbols, Eq, solve
+from sympy import Eq, symbols, solve
 
-st.title("Sample Streamlit App")
+st.title("Algebraic Equation Solver")
 
-# Read the dataset
-@st.cache  # Use caching to improve performance
-def load_data():
-    data = pd.read_csv('data/dataset.csv')
-    return data
-
-data = load_data()
-
-st.header("Sample Dataset")
-st.write(data)
-
-st.header("Algebraic Equation Solver")
-st.write("Solve an equation of the form 'ax + b = 0'")
+st.write("Enter an algebraic equation of the form 'ax = b', 'ax + b = c', or 'ax + bx = c':")
 
 # User input for the equation
-a = st.number_input("Enter the value of 'a':")
-b = st.number_input("Enter the value of 'b':")
+equation_input = st.text_input("Equation:", value="2x + 2 = 4")
 
-equation = Eq(a * symbols('x') + b, 0)
+# Parse the equation
+parts = equation_input.split('=')
+if len(parts) != 2:
+    st.error("Invalid equation format. Please use 'ax = b', 'ax + b = c', or 'ax + bx = c'.")
+else:
+    left_part = parts[0].strip()
+    right_part = parts[1].strip()
 
-if st.button("Solve Equation"):
+    # Define symbols
+    x = symbols('x')
+    
     try:
-        solution = solve(equation, symbols('x'))
+        if '+' in left_part:
+            left_terms = left_part.split('+')
+            if len(left_terms) == 2:
+                a, b = [term.strip() for term in left_terms]
+                equation = Eq(float(a) * x + float(b), float(right_part))
+            else:
+                st.error("Invalid equation format. Please use 'ax = b', 'ax + b = c', or 'ax + bx = c'.")
+        elif left_part.count('x') == 1:
+            a = left_part.replace('x', '').strip()
+            equation = Eq(float(a) * x, float(right_part))
+        else:
+            st.error("Invalid equation format. Please use 'ax = b', 'ax + b = c', or 'ax + bx = c'.")
+        
+        if not st.session_state.get('eq') or st.session_state.eq != equation:
+            st.session_state.eq = equation
+
+        solution = solve(equation, x)
         st.write(f"Solutions for x: {solution}")
     except Exception as e:
         st.error("Invalid Equation")
