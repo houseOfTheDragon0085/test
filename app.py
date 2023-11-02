2905c8dc7de9364ebaf83c2428f4f056aa6038ed
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-02 22:32:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ import numpy as np
 
 st.title("Linear Equation Solver")
 
-st.write("Enter a linear system of equations as matrices (e.g., '[[3, -9], [2, 4]] * [x, y] = [-42, 2]', '[[1, -2, -1], [2, 2, -1], [-1, -1, 2]] * [x, y, z] = [6, 1, 1]'):")
+st.write("Enter a linear system of equations as space-separated values (e.g., '3 -9; 2 4' * 'x y' = '-42 2', '1 -2 -1; 2 2 -1; -1 -1 2' * 'x y z' = '6 1 1'):")
 
 # User input for the equation
 equation_input = st.text_input("Linear System of Equations:")
 
 try:
     # Split the input into matrices and vectors
-    equations = equation_input.split('=')
-    coefficients, constants = equations[0].strip(), equations[1].strip()
+    matrices, variables_and_constants = equation_input.split('*')
+
+    coefficients = [list(map(float, matrix.strip().split())) for matrix in matrices.split(';')]
+    constants = list(map(float, variables_and_constants.split('='))
 
     # Parse matrices and vectors
-    A = np.array(eval(coefficients))
-
-    b = np.array(eval(constants)
+    A = np.array(coefficients)
+    b = np.array(constants)
 
     # Solve the linear system of equations
     x = np.linalg.solve(A, b)
