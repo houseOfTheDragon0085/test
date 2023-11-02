cca0eb93154a52dc6693403b40a0455af254d5a4
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-02 22:52:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 import streamlit as st
 import sympy as sym
 
-st.title("Algebraic Equation Solver with SymPy")
+st.title("Symbolic Equation Solver with SymPy")
 
-st.write("Enter an algebraic equation with symbolic variables (e.g., '2*x**2 + y + z = 1', 'x + 2*y + z = c1', '-2*x + y = -z'):")
+st.write("Enter algebraic equations with symbolic variables in separate fields:")
 
-# User input for the equation
-equation_input = st.text_input("Equation:")
+# User input for the equations
+equation_x = st.text_input("Equation for x:")
+equation_y = st.text_input("Equation for y:")
+equation_z = st.text_input("Equation for z:")
 
 # Define symbolic variables
 x, y, z, c1 = sym.symbols('x y z c1')
 
 try:
-    # Parse and solve the equation
-    equations = [sym.Eq(*sym.sympify(eq.split('='))) for eq in equation_input.split(',')]
-    solutions = sym.solve(equations, (x, y, z))
+    # Parse and solve the equations
+    eq_x = sym.Eq(*sym.sympify(equation_x.split('=')))
+    eq_y = sym.Eq(*sym.sympify(equation_y.split('=')))
+    eq_z = sym.Eq(*sym.sympify(equation_z.split('=')))
 
-    st.write("Solutions for x, y, z:")
-    for solution in solutions:
-        st.write(solution)
+    solution_x = sym.solve(eq_x, x)
+    solution_y = sym.solve(eq_y, y)
+    solution_z = sym.solve(eq_z, z)
+
+    st.write("Solutions:")
+    st.write(f"x = {solution_x}")
+    st.write(f"y = {solution_y}")
+    st.write(f"z = {solution_z}")
 except Exception as e:
-    st.error("Invalid Equation")
+    st.error("Invalid Equations")
