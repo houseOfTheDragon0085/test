b3f43e1c8c9b02306050014d98b26dc5931d3282
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-02 22:04:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 import streamlit as st
-from sympy import symbols, Eq, solve
+from scipy.optimize import fsolve
+import numpy as np
 
-st.title("Algebraic Equation Solver")
+st.title("Nonlinear Equation Solver")
 
-st.write("Enter an algebraic equation (e.g., '2x + 2 = 4', '2x = 2', '2x + 2x = 4'):")
+st.write("Enter a system of equations in the form of F(x, y, w) = 0")
 
-# User input for the equation
-equation_input = st.text_input("Equation:")
+# User input for the equations
+equation1 = st.text_input("Equation 1 (e.g., 'x**2 + y**2 - 20 = 0'):")
+equation2 = st.text_input("Equation 2 (e.g., 'y - x**2 = 0'):")
+equation3 = st.text_input("Equation 3 (e.g., 'w + 5 - x*y = 0'):")
 
-# Define a symbolic variable
-x = symbols('x')
+def myFunction(z):
+    x = z[0]
+    y = z[1]
+    w = z[2]
 
-try:
-    # Parse and solve the equation
-    equation = Eq(eval(equation_input.replace('x', '*x')), 0)
-    solution = solve(equation, x)
+    F = np.empty((3))
+    F[0] = eval(equation1)  # Parse and evaluate user input as the first equation
+    F[1] = eval(equation2)  # Parse and evaluate user input as the second equation
+    F[2] = eval(equation3)  # Parse and evaluate user input as the third equation
+    return F
 
-    st.write(f"Solutions for x: {solution}")
-except Exception as e:
-    st.error("Invalid Equation")
+zGuess = np.array([1, 1, 1])
+
+if st.button("Solve Equations"):
+    z = fsolve(myFunction, zGuess)
+    st.write(f"Solutions for x, y, w: {z}")
