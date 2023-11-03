925d9ff627bc284579e409892774df39aac35dea
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 10:08:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,49 +10,48 @@ st.title("Algebraic Expression Solver")
 st.write("Enter an algebraic expression (e.g., 'x + y + x - y', '2x + 2x + 2y - y'):")
 
 # User input for the expression
-expression_input = st.text_input("Expression:")
+expression_input = st.chat_input("User", "Type an expression...")
 
 # Define symbolic variables
 x, y = sym.symbols('x y')
 
-try:
-    # Parse the expression
-    parsed_expr = sym.sympify(expression_input)
+if expression_input:
+    try:
+        # Parse the expression
+        parsed_expr = sym.sympify(expression_input)
 
-    # Display the original expression
-    st.write(f"Original expression: {parsed_expr}")
+        # Display the original expression
+        st.write(f"User: {expression_input}")
 
-    # Initialize a list to store solving steps
-    solving_steps = [parsed_expr]
+        # Initialize a list to store solving steps
+        solving_steps = [parsed_expr]
 
-    # Simplify the expression step by step
-    simplified_expr = parsed_expr
-    while True:
-        simplified = sym.simplify(simplified_expr)
-        if simplified == simplified_expr:
-            break  # No further simplification possible
-        simplified_expr = simplified
-        solving_steps.append(simplified_expr)
+        # Simplify the expression step by step
+        simplified_expr = parsed_expr
+        while True:
+            simplified = sym.simplify(simplified_expr)
+            if simplified == simplified_expr:
+                break  # No further simplification possible
+            simplified_expr = simplified
+            solving_steps.append(simplified_expr)
 
-    # Display step-by-step solving
-    st.write("Solving steps:")
-    for i, step in enumerate(solving_steps):
-        st.write(f"Step {i + 1}: {step}")
+        # Display step-by-step solving
+        st.write("Solving steps:")
+        for i, step in enumerate(solving_steps):
+            st.write(f"Step {i + 1}: {step}")
 
-    st.write(f"Final simplified expression: {simplified_expr}")
+        st.write(f"Solver: {simplified_expr}")
 
-    # Store the expression and the simplified result
-    st.session_state.data.append((expression_input, simplified_expr))
+        # Store the expression and the simplified result
+        st.session_state.data.append((expression_input, simplified_expr))
 
-except Exception as e:
-    st.error("Invalid Expression")
+    except Exception as e:
+        st.error("Invalid Expression")
 
 # Display previous inserts and answers in a chat-like format
 st.write("Chat History:")
 
 if st.session_state.data:
     for i, (expression, answer) in enumerate(st.session_state.data):
-        st.beta_container()
-        st.text(f"User: {expression}")
-        st.text(f"Solver: {answer}")
-        st.beta_container()
+        st.write(f"User: {expression}")
+        st.write(f"Solver: {answer}")
