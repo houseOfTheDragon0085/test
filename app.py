bdc3e37b0e3b06aff8edd68d2e9e1f835c8d5222
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 10:02:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import sympy as sym
 
+# Initialize a session_state variable to store data
+if 'data' not in st.session_state:
+    st.session_state.data = []
+
 st.title("Algebraic Expression Solver")
 
 st.write("Enter an algebraic expression (e.g., 'x + y + x - y', '2x + 2x + 2y - y'):")
@@ -9,7 +13,7 @@ st.write("Enter an algebraic expression (e.g., 'x + y + x - y', '2x + 2x + 2y - 
 expression_input = st.text_input("Expression:")
 
 # Define symbolic variables
-x, y, z, a, b = sym.symbols('x y z a b')
+x, y = sym.symbols('x y')
 
 try:
     # Parse the expression
@@ -21,14 +25,14 @@ try:
     # Initialize a list to store solving steps
     solving_steps = [parsed_expr]
 
-    # Get the variables in the expression
-    variables = list(parsed_expr.free_symbols)
-
-    # Simplify the expression step by step for each variable
-    for var in variables:
-        simplified_expr = sym.simplify(parsed_expr, rational=True)
+    # Simplify the expression step by step
+    simplified_expr = parsed_expr
+    while True:
+        simplified = sym.simplify(simplified_expr)
+        if simplified == simplified_expr:
+            break  # No further simplification possible
+        simplified_expr = simplified
         solving_steps.append(simplified_expr)
-        parsed_expr = simplified_expr
 
     # Display step-by-step solving
     st.write("Solving steps:")
@@ -37,5 +41,14 @@ try:
 
     st.write(f"Final simplified expression: {simplified_expr}")
 
+    # Store the expression and the simplified result
+    st.session_state.data.append((expression_input, simplified_expr))
+
 except Exception as e:
     st.error("Invalid Expression")
+
+# Display a table with previous inserts and answers
+st.write("Previous Inserts and Answers:")
+if st.session_state.data:
+    for i, (expression, answer) in enumerate(st.session_state.data):
+        st.write(f"Insert {i + 1}: Expression: {expression}, Answer: {answer}")
