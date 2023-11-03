a39bad81560d86867bc5776ca66f7abdbab98ad2
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 08:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,42 @@
 import streamlit as st
 import sympy as sym
-from chatbot import chatbot  # Import your chatbot function
+import spacy
+
+# Load the spaCy NLP model
+nlp = spacy.load("en_core_web_sm")
+
+st.title("Algebraic Expression Solver")
+
+st.write("Enter a sentence or question with an algebraic expression (e.g., 'Hi, can you solve 2x + 3 = 8', 'Calculate 2x + 2x'):")
+
+# User input for the sentence/question
+user_input = st.text_input("Question:")
 
 # Define symbolic variables
 x = sym.Symbol('x')
-y = sym.Symbol('y')
 
-# Function to extract and solve algebraic expressions
-def solve_algebraic_expression(expression):
-    try:
-        parsed_expr = sym.sympify(expression, evaluate=False)
+try:
+    # Use spaCy to extract algebraic expressions from user input
+    doc = nlp(user_input)
+
+    # Initialize an empty list to collect extracted expressions
+    expressions = []
+
+    for token in doc:
+        if token.pos_ == "SYM":
+            # Collect symbols to form expressions
+            expressions.append(str(token))
+
+    if expressions:
+        # Combine the collected symbols to form an expression
+        expression = " ".join(expressions)
+
+        # Parse and solve the expression
+        parsed_expr = sym.sympify(expression)
         simplified_expr = sym.simplify(parsed_expr)
-        return simplified_expr
-    except Exception as e:
-        return None
 
-st.title("Algebraic Expression Solver with Chatbot")
-
-st.write("Enter a chat message with an algebraic expression (e.g., 'Can you help me solve 2x + 2 = 4?'):")
-
-# User input for the chat message
-chat_input = st.text_input("Chatbot Message:")
-
-if not chat_input:
-    st.info("Please enter a chat message.")
-else:
-    # Check if the chat input contains a number
-    if any(char.isdigit() for char in chat_input):
-        # Extract and solve the algebraic expression
-        simplified_expr = solve_algebraic_expression(chat_input)
-
-        if simplified_expr:
-            st.write(f"Algebraic Expression: {simplified_expr}")
-            st.write(f"Solution: {simplified_expr}")
-        else:
-            st.error("Invalid Algebraic Expression")
-
+        st.write(f"Simplified expression: {simplified_expr}")
     else:
-        # If the chat input doesn't contain numbers, run the chatbot and fetch the reply
-        st.info("Fetching chatbot reply...")
-        chatbot_reply = chatbot(chat_input)
-        st.write("Chatbot Reply:")
-        st.write(chatbot_reply)
+        st.error("No algebraic expression found in the input.")
+except Exception as e:
+    st.error("Invalid Input")
