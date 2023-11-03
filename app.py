1da0597cc8d5fb256c51b1ceeacc4dbc8887eedf
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 09:30:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import sympy as sym
-import spacy
-import en_core_web_sm
-
-
-# Load the spaCy NLP model
-nlp = en_core_web_sm.load()
+import re
 
 st.title("Algebraic Expression Solver")
 
@@ -18,26 +13,24 @@ user_input = st.text_input("Question:")
 x = sym.Symbol('x')
 
 try:
-    # Use spaCy to extract algebraic expressions from user input
-    doc = nlp(user_input)
+    # Use regular expressions to extract algebraic expressions from user input
+    matches = re.findall(r'(\d*\.*\d*\s*[a-zA-Z]\s*[\+\-\*/\(\)\^=\s]*)+', user_input)
 
-    # Initialize an empty list to collect extracted expressions
-    expressions = []
-
-    for token in doc:
-        if token.pos_ == "SYM":
-            # Collect symbols to form expressions
-            expressions.append(str(token))
-
-    if expressions:
-        # Combine the collected symbols to form an expression
-        expression = " ".join(expressions)
+    if matches:
+        # Combine the extracted matches to form an expression
+        expression = " ".join(matches)
 
         # Parse and solve the expression
         parsed_expr = sym.sympify(expression)
         simplified_expr = sym.simplify(parsed_expr)
+        solution = sym.solve(simplified_expr, x)
 
+        st.write(f"Original expression: {parsed_expr}")
         st.write(f"Simplified expression: {simplified_expr}")
+        st.write(f"Solving steps:")
+        for step in sym.steps(solution):
+            st.write(step)
+        st.write(f"Solution for x: {solution}")
     else:
         st.error("No algebraic expression found in the input.")
 except Exception as e:
