3b06a4942a9c6725a5d404aba47bb04af4bae768
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 07:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 import streamlit as st
 import sympy as sym
-
-st.title("Algebraic Expression Solver")
-
-st.write("Enter an algebraic expression (e.g., 'x + y + x - y', '2x + 2y'):")
-
-# User input for the expression
-expression_input = st.text_input("Expression:")
+from chatbot import chatbot  # Import your chatbot function
 
 # Define symbolic variables
 x = sym.Symbol('x')
 y = sym.Symbol('y')
 
-try:
-    # Parse the expression
-    parsed_expr = sym.sympify(expression_input)
+# Function to extract and solve algebraic expressions
+def solve_algebraic_expression(expression):
+    try:
+        parsed_expr = sym.sympify(expression, evaluate=False)
+        simplified_expr = sym.simplify(parsed_expr)
+        return simplified_expr
+    except Exception as e:
+        return None
 
-    # Simplify the expression
-    simplified_expr = sym.simplify(parsed_expr)
+st.title("Algebraic Expression Solver with Chatbot")
 
-    st.write(f"Simplified expression: {simplified_expr}")
-except Exception as e:
-    st.error("Invalid Expression")
+st.write("Enter a chat message with an algebraic expression (e.g., 'Can you help me solve 2x + 2 = 4?'):")
+
+# User input for the chat message
+chat_input = st.text_input("Chatbot Message:")
+
+if not chat_input:
+    st.info("Please enter a chat message.")
+else:
+    # Check if the chat input contains a number
+    if any(char.isdigit() for char in chat_input):
+        # Extract and solve the algebraic expression
+        simplified_expr = solve_algebraic_expression(chat_input)
+
+        if simplified_expr:
+            st.write(f"Algebraic Expression: {simplified_expr}")
+            st.write(f"Solution: {simplified_expr}")
+        else:
+            st.error("Invalid Algebraic Expression")
+
+    else:
+        # If the chat input doesn't contain numbers, run the chatbot and fetch the reply
+        st.info("Fetching chatbot reply...")
+        chatbot_reply = chatbot(chat_input)
+        st.write("Chatbot Reply:")
+        st.write(chatbot_reply)
