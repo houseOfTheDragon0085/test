d05bfe83f495f1c8006963feea70b892f5dfe82a
houseOfTheDragon0085 <149698546+houseOfTheDragon0085@users.noreply.github.com>
2023-11-03 09:49:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,32 @@
 import streamlit as st
 import sympy as sym
-import re
 
 st.title("Algebraic Expression Solver")
 
-st.write("Enter a sentence or question with an algebraic expression following a colon (e.g., 'Please calculate: 2x + 3 = 8', 'Solve: 2x + 2x'):")
+st.write("Enter an algebraic expression (e.g., 'x + y + x - y', '2x + 2y'):")
 
-# User input for the sentence/question
-user_input = st.text_input("Question:")
+# User input for the expression
+expression_input = st.text_input("Expression:")
 
 # Define symbolic variables
 x, y = sym.symbols('x y')
 
 try:
-    # Use regular expressions to extract algebraic expressions following a colon
-    matches = re.findall(r':\s*(\d*\.*\d*\s*[a-zA-Z]\s*[\+\-\*/\(\)\^=\s]*)+', user_input)
+    # Parse the expression
+    parsed_expr = sym.sympify(expression_input)
 
-    if matches:
-        # Combine the extracted matches to form an expression
-        expression = " ".join(matches)
+    # Simplify the expression
+    simplified_expr = sym.simplify(parsed_expr)
 
-        # Remove the colon and leading white spaces
-        expression = expression.lstrip(':').strip()
+    st.write(f"Original expression: {parsed_expr}")
+    st.write(f"Simplified expression: {simplified_expr}")
 
-        # Replace '*' with Python-friendly multiplication symbol '**'
-        expression = expression.replace('*', '**')
+    # Solve the expression and get step-by-step solutions
+    solutions = sym.solveset(simplified_expr, (x, y), domain=sym.S.Reals)
+    
+    st.write("Solving steps:")
+    for solution in solutions:
+        st.write(solution)
 
-        # Parse the expression
-        parsed_expr = sym.sympify(expression)
-        simplified_expr = sym.simplify(parsed_expr)
-
-        # Solve the expression
-        solution = sym.solve(parsed_expr, (x, y))
-
-        st.write(f"Original expression: {parsed_expr}")
-        st.write(f"Simplified expression: {simplified_expr}")
-
-        st.write("Solving steps:")
-        for step in sym.solve(parsed_expr, (x, y), manual=True):
-            st.write(step)
-
-        st.write(f"Solution for x and y: {solution}")
-    else:
-        st.error("No algebraic expression found in the input.")
 except Exception as e:
-    st.error("Invalid Input")
+    st.error("Invalid Expression")
